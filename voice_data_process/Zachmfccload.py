cff2b522c7888f554f0e217cd417617bc9bd995f
maxwellwli <lnelog10@126.com>
2017-11-20 20:54:13 +0800

    提交语音处理文件

---
+++ voice_data_process/Zachmfccload.py
@@ -0,0 +1,13 @@
+from __future__ import print_function
+import numpy as np
+import scipy
+import matplotlib.pyplot as plt
+import librosa
+import librosa.display
+import matplotlib.pyplot as plt
+from pydub import AudioSegment
+
+dirname = "audio/Keeping Up with the Joneses Interview/"
+a=np.loadtxt(dirname+"0.txt")
+print(a)
+
