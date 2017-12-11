46e8a966d190515d91df9e3706b738c9b6e81c68
jackform <296256067@qq.com>
2017-12-11 21:29:12 +0800

    fix sampler problem

--- speech2vivi.py
+++ speech2vivi.py
@@ -97,11 +97,11 @@ class speech2vivi(object):
 
         self.sample_random_image = tf.placeholder(tf.float32,
                                            [1, self.image_size, self.image_size, self.input_c_dim],
-                                           name='random_image_real_data')
+                                           name='sample_random_image_real_data')
 
         self.sample_real_voice = tf.placeholder(tf.float32,
                                          [1, self.voice_dimen, self.voice_time, 1],
-                                         name='mouth_voice_real_data')
+                                         name='sample_mouth_voice_real_data')
 
         print("random shape",self.random_image.shape)
 
@@ -437,12 +437,12 @@ class speech2vivi(object):
                 self.writer.add_summary(summary_str, counter)
 
                 # Update G network
-                _, summary_str = self.sess.run([g_optim, self.merged],
+                _, summary_str = self.sess.run([g_optim, self.g_sum],
                                                feed_dict={ self.real_image: batch_images, self.real_voice:voiceData, self.random_image: batch_specific_images })
                 self.writer.add_summary(summary_str, counter)
 
                 # Run g_optim twice to make sure that d_loss does not go to zero (different from paper)
-                _, summary_str = self.sess.run([g_optim, self.merged],
+                _, summary_str = self.sess.run([g_optim, self.g_sum],
                                                feed_dict={ self.real_image: batch_images, self.real_voice:voiceData, self.random_image: batch_specific_images })
                 self.writer.add_summary(summary_str, counter)
 
@@ -569,91 +569,114 @@ class speech2vivi(object):
             # for audio
             # voice = tf.constant(1.0, shape=[self.batch_size, 13, 35, 1])
 
-            v_conv1 = conv2d(voice, 64, 3, 3, 1, 1, name="g_v_conv1",no_summery=True)
-            # v_e1 = self.g_bn_v_e1(v_conv1)
-            # v_conv2 = conv2d(v_e1, 128, 3, 3, 1, 1, name="g_v_conv2")
-            v_conv2 = conv2d(v_conv1, 128, 3, 3, 1, 1, name="g_v_conv2",no_summery=True)
-            # v_e2 = self.g_bn_v_e2(v_conv2)
-            # v_max_pool2 = max_pool_3x3_2t(v_e2)
-            v_max_pool2 = max_pool_3x3_2t(v_conv2)
-            v_conv3 = conv2d(v_max_pool2, 256, 3, 3, 1, 1, name="g_v_conv3",no_summery=True)
-            # v_e3 = self.g_bn_v_e3(v_conv3)
-            # v_conv4 = conv2d(v_e3, 256, 3, 3, 1, 1, name="g_v_conv4")
-            v_conv4 = conv2d(v_conv3, 256, 3, 3, 1, 1, name="g_v_conv4",no_summery=True)
-            # v_e4 = self.g_bn_v_e4(v_conv4)
-            # v_conv5 = conv2d(v_e4, 512, 3, 3, 1, 1, name="g_v_conv5")
-            v_conv5 = conv2d(v_conv4, 512, 3, 3, 1, 1, name="g_v_conv5",no_summery=True)
-            # v_e5 = self.g_bn_v_e5(v_conv5)
-            # v_max_pool2 = max_pool_3x3_2t(v_e5)
-            v_max_pool2 = max_pool_3x3_2t(v_conv5)
-            v_fc6 = linear(tf.reshape(v_max_pool2, [batch_size, -1]), 512, "g_v_fc6",no_summery=True)
-            v_fc7 = linear(tf.reshape(v_fc6, [batch_size, -1]), 256, "g_v_fc7",no_summery=True)
+            v_conv1 = conv2d(voice, 64, 3, 3, 1, 1, name="g_v_conv1")
+            # v_e1 = lrelu(v_conv1)
+            v_e1 = v_conv1
+            v_conv2 = conv2d(v_e1, 128, 3, 3, 1, 1, name="g_v_conv2")
+            # v_e2 = lrelu(self.g_bn_v_e2(v_conv2))
+            # v_e2 = lrelu(v_conv2)
+            v_e2 = v_conv2
+            v_max_pool2 = max_pool_3x3_2t(v_e2)
+            v_conv3 = conv2d(v_max_pool2, 256, 3, 3, 1, 1, name="g_v_conv3")
+            # v_e3 = lrelu(self.g_bn_v_e3(v_conv3))
+            # v_e3 = lrelu(v_conv3)
+            v_e3 = v_conv3
+            v_conv4 = conv2d(v_e3, 256, 3, 3, 1, 1, name="g_v_conv4")
+            # v_e4 = lrelu(self.g_bn_v_e4(v_conv4))
+            # v_e4 = lrelu(v_conv4)
+            v_e4 = v_conv4
+            v_conv5 = conv2d(v_e4, 512, 3, 3, 1, 1, name="g_v_conv5")
+            # v_e5 = lrelu(self.g_bn_v_e5(v_conv5))
+            # v_e5 = lrelu(v_conv5)
+            v_e5 = v_conv5
+            v_max_pool2 = max_pool_3x3_2t(v_e5)
+            # v_max_pool2 = max_pool_3x3_2t(v_conv5)
+            # v_fc6 = lrelu(linear(tf.reshape(v_max_pool2, [self.batch_size, -1]), 512, "g_v_fc6"))
+            v_fc6 = linear(tf.reshape(v_max_pool2, [batch_size, -1]), 512, "g_v_fc6")
+            # v_fc7 = lrelu(linear(tf.reshape(v_fc6, [self.batch_size, -1]), 256, "g_v_fc7"))
+            v_fc7 = linear(tf.reshape(v_fc6, [batch_size, -1]), 256, "g_v_fc7")
 
             # for video
             # image = tf.constant(1.0, shape=[self.batch_size, 112, 112, 3])
 
-            i_conv1 = conv2d(image, 96, 7, 7, 2, 2, name="g_i_conv1",no_summery=True)
+            i_conv1 = conv2d(image, 96, 7, 7, 2, 2, name="g_i_conv1")
             # i_e1 = self.g_bn_i_e1(i_conv1)
-            i_maxPool1 = max_pool_3x3_2(i_conv1)
+            # i_e1 = lrelu(i_conv1, name="g_i_e1")
+            i_e1 = i_conv1
+            i_maxPool1 = max_pool_3x3_2(i_e1)
 
-            i_conv2 = conv2d_valid(i_maxPool1, 256, 5, 5, 2, 2, name="g_i_conv2",no_summery=True)
+            i_conv2 = conv2d_valid(i_maxPool1, 256, 5, 5, 2, 2, name="g_i_conv2")
             # i_e2 = self.g_bn_i_e2(i_conv2)
-            i_maxPool2 = max_pool_3x3_2(i_conv2)
+            # i_e2 = lrelu(i_conv2)
+            i_e2 = i_conv2
+            i_maxPool2 = max_pool_3x3_2(i_e2)
 
-            i_conv3 = conv2d(i_maxPool2, 512, 3, 3, 1, 1, name="g_i_conv3",no_summery=True)
-            # i_e3 = self.g_bn_i_e3(i_conv3)
-            # i_conv4 = conv2d(i_e3, 512, 3, 3, 1, 1, name="g_i_conv4")
-            i_conv4 = conv2d(i_conv3, 512, 3, 3, 1, 1, name="g_i_conv4",no_summery=True)
-            # i_e4 = self.g_bn_i_e4(i_conv4)
-            # i_conv5 = conv2d(i_e4, 512, 3, 3, 1, 1, name="g_i_conv5")
-            i_conv5 = conv2d(i_conv4, 512, 3, 3, 1, 1, name="g_i_conv5",no_summery=True)
-            # i_e5 = self.g_bn_i_e5(i_conv5)
+            i_conv3 = conv2d(i_maxPool2, 512, 3, 3, 1, 1, name="g_i_conv3")
+            # i_e3 = lrelu(self.g_bn_i_e3(i_conv3))
+            # i_e3 = lrelu(i_conv3)
+            i_e3 = i_conv3
+            i_conv4 = conv2d(i_e3, 512, 3, 3, 1, 1, name="g_i_conv4")
+            # i_e4 = lrelu(self.g_bn_i_e4(i_conv4))
+            # i_e4 = lrelu(i_conv4)
+            i_e4 = i_conv4
+            i_conv5 = conv2d(i_e4, 512, 3, 3, 1, 1, name="g_i_conv5")
+            # i_e5 = lrelu(self.g_bn_i_e5(i_conv5))
+            # i_e5 = lrelu(i_conv5)
+            i_e5 = i_conv5
 
-            # i_reshape1 = tf.reshape(i_e5, [self.batch_size, -1])
-            i_reshape1 = tf.reshape(i_conv5, [batch_size, -1])
-            i_fc6 = linear(i_reshape1, 512, "g_i_fc6",no_summery=True)
-            i_fc7 = linear(i_fc6, 256, "g_i_fc7",no_summery=True)
+            i_reshape1 = tf.reshape(i_e5, [batch_size, -1])
+            # i_fc6 = lrelu(linear(i_reshape1, 512, "g_i_fc6"))
+            i_fc6 = linear(i_reshape1, 512, "g_i_fc6")
+            # i_fc7 = lrelu(linear(i_fc6, 256, "g_i_fc7"))
+            i_fc7 = linear(i_fc6, 256, "g_i_fc7")
 
             # concat
 
+            print("v_fc7_shape", v_fc7.shape)
+            print("i_fc7_shape", i_fc7.shape)
             i_v_concat = tf.concat([v_fc7, i_fc7], 1)
+            print("i_v_concat_shape", i_v_concat.shape)
 
             # generate new picture
-            i_v_fc1 = linear(i_v_concat, 128, "g_i_v_fc1",no_summery=True)
-            i_v_convT2 = deconv2d(tf.reshape(i_v_fc1, [-1, 2, 2, 32]), [batch_size, 4, 4, 512], 6, 6, 2, 2,
-                                  name="g_i_v_convT2",no_summery=True)
-            # i_v_e2 = self.g_bn_i_v_e2(i_v_convT2, 0.5)
-            # i_v_e2 = tf.nn.dropout(self.g_bn_i_v_e2(i_v_convT2,0.5))
-            # i_v_convT3 = deconv2d_valid(i_v_e2, [self.batch_size, 12, 12, 256], 5, 5, 2, 2, name="g_i_v_convT3")
-            i_v_convT3 = deconv2d_valid(i_v_convT2, [batch_size, 12, 12, 256], 5, 5, 2, 2, name="g_i_v_convT3",no_summery=True)
-            # i_v_e3 = self.g_bn_i_v_e3(i_v_convT3, 0.5)
-            # i_v_e3 = tf.nn.dropout(self.g_bn_i_v_e3(i_v_convT3,0.5))
-            # i_v_concat1 = tf.concat([i_conv2, i_v_e3], 3, name="g_i_v_concat1")
-            # i_v_concat1 = tf.concat([i_conv2, i_v_convT3], 3, name="g_i_v_concat1")
-            # i_v_concat1 = tf.concat([i_conv2, i_v_convT3], 3, name="g_i_v_concat1")
-            i_v_concat1 = tf.concat([i_v_convT3, i_v_convT3], 3, name="g_i_v_concat1")
-            i_v_convT4 = deconv2d_valid(i_v_concat1, [batch_size, 28, 28, 96], 5, 5, 2, 2, name="g_i_v_convT4",no_summery=True)
-            # i_v_e4 = self.g_bn_i_v_e4(i_v_convT4)
-            # i_v_e4 = tf.nn.dropout(self.g_bn_i_v_e4(i_v_convT4,0.5))
-            # i_v_concat2 = tf.concat([i_maxPool1, i_v_e4], 3, name="g_i_v_concat2")
-            # i_v_concat2 = tf.concat([i_maxPool1, i_v_convT4], 3, name="g_i_v_concat2")
-            i_v_concat2 = tf.concat([i_v_convT4, i_v_convT4], 3, name="g_i_v_concat2")
+            i_v_fc1 = linear(i_v_concat, 128, "g_i_v_fc1")
+            # i_v_convT2 = deconv2d(tf.reshape(self.g_bn_i_v_e1(lrelu(i_v_fc1)), [-1, 2, 2, 32]), [self.batch_size, 4, 4, 512], 6, 6, 2, 2, name="g_i_v_convT2")
+            i_v_convT2 = deconv2d(tf.reshape(i_v_fc1, [-1, 2, 2, 32]), [self.batch_size, 4, 4, 512], 6, 6, 2, 2,
+                                  name="g_i_v_convT2")
+            # i_v_e2 = tf.nn.dropout(self.g_bn_i_v_e2(lrelu(i_v_convT2)),0.5)
+            # i_v_e2 = tf.nn.dropout(lrelu(i_v_convT2),0.5)
+            # i_v_e2 = lrelu(i_v_convT2)
+            i_v_e2 = i_v_convT2
+            i_v_convT3 = deconv2d_valid(i_v_e2, [batch_size, 12, 12, 256], 5, 5, 2, 2, name="g_i_v_convT3")
+            # i_v_e3 = tf.nn.dropout(self.g_bn_i_v_e3(lrelu(i_v_convT3)),0.5)
+            # i_v_e3 = tf.nn.dropout(lrelu(i_v_convT3),0.5)
+            # i_v_e3 = lrelu(i_v_convT3)
+            i_v_e3 = i_v_convT3
+            i_v_concat1 = tf.concat([i_conv2, i_v_e3], 3, name="g_i_v_concat1")
+            i_v_convT4 = deconv2d_valid(i_v_concat1, [batch_size, 28, 28, 96], 5, 5, 2, 2, name="g_i_v_convT4")
+            # i_v_e4 = tf.nn.dropout(self.g_bn_i_v_e4(lrelu(i_v_convT4)),0.5)
+            # i_v_e4 = tf.nn.dropout(lrelu(i_v_convT4),0.5)
+            # i_v_e4 = lrelu(i_v_convT4)
+            i_v_e4 = i_v_convT4
+            i_v_concat2 = tf.concat([i_maxPool1, i_v_e4], 3, name="g_i_v_concat2")
             # for convinient to compare with original size, change to 112*112
-            i_v_convT5 = deconv2d(i_v_concat2, [batch_size, 56, 56, 96], 5, 5, 2, 2, name="g_i_v_convT5",no_summery=True)
-            # i_v_e5 = self.g_bn_i_v_e5(i_v_convT5)
-            # i_v_e5 = tf.nn.dropout(self.g_bn_i_v_e5(i_v_convT5,0.5))
-            # i_v_convT6 = deconv2d(i_v_e5, [self.batch_size, 112, 112, 64], 5, 5, 2, 2, name="g_i_v_convT6")
-            i_v_convT6 = deconv2d(i_v_convT5, [batch_size, 112, 112, 64], 5, 5, 2, 2, name="g_i_v_convT6",no_summery=True)
-            # i_v_e6 = self.g_bn_i_v_e6(i_v_convT6
-            # i_v_e6 = tf.nn.dropout(self.g_bn_i_v_e6(i_v_convT6,0.5))
-            # i_v_convT7 = deconv2d(i_v_e6, [self.batch_size, 112, 112, 3], 5, 5, 1, 1, name="g_i_v_convT7")
-            i_v_convT7 = deconv2d(i_v_convT6, [batch_size, 112, 112, 3], 5, 5, 1, 1, name="g_i_v_convT7",no_summery=True)
-            # i_v_e7 = self.g_bn_i_v_e7(i_v_convT7)
-            # i_v_e7 = tf.nn.dropout(self.g_bn_i_v_e7(i_v_convT7,0.5))
+            i_v_convT5 = deconv2d(i_v_concat2, [batch_size, 56, 56, 96], 5, 5, 2, 2, name="g_i_v_convT5")
+            # i_v_e5 = tf.nn.dropout(self.g_bn_i_v_e5(lrelu(i_v_convT5)),0.5)
+            # i_v_e5 = tf.nn.dropout(lrelu(i_v_convT5),0.5)
+            # i_v_e5 = lrelu(i_v_convT5)
+            i_v_e5 = i_v_convT5
+            i_v_convT6 = deconv2d(i_v_e5, [batch_size, 112, 112, 64], 5, 5, 2, 2, name="g_i_v_convT6")
+            # i_v_e6 = tf.nn.dropout(self.g_bn_i_v_e6(lrelu(i_v_convT6)),0.5)
+            # i_v_e6 = tf.nn.dropout(lrelu(i_v_convT6),0.5)
+            # i_v_e6 = lrelu(i_v_convT6)
+            i_v_e6 = i_v_convT6
+            i_v_convT7 = deconv2d(i_v_e6, [batch_size, 112, 112, 3], 5, 5, 1, 1, name="g_i_v_convT7")
+            # i_v_e7 = tf.nn.dropout(self.g_bn_i_v_e7(lrelu(i_v_convT7)),0.5)
+            # i_v_e7 = tf.nn.dropout(lrelu(i_v_convT7),0.5)
+            # i_v_e7 = lrelu(i_v_convT7)
+            i_v_e7 = i_v_convT7
 
             # print("generator's shape:",i_v_e7.shape)
-            # return i_v_e7
-            return i_v_convT7
+            return i_v_e7
 
     def load(self, checkpoint_dir):
         print(" [*] Reading checkpoint...")
